7a291123f6df7aa52ff876f0a159d3d2bb574a39
william <wpelletier.development@yahoo.com>
2025-04-26 10:42:01 -0400

    default opentk setup to test

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Modulus;
+
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var nativeWindowSettings = new NativeWindowSettings()
+        {
+            ClientSize = new Vector2i(800, 600),
+            Title = "Modulus testing",
+            Flags = ContextFlags.ForwardCompatible,
+        };
+
+        Window window = new Window(GameWindowSettings.Default, nativeWindowSettings);
+
+        window.Run();
+
+    }
+}
